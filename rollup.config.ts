983f0d247d598c7d9ae96010d85eec07ef78d748
Exponential-Workload <cc@aaathats3as.com>
2023-08-30 04:30:16 +0200

    feat: Add CJS support

---
+++ rollup.config.ts
@@ -0,0 +1,29 @@
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser'
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.js',
+        format: 'esm',
+        sourcemap: true,
+      }
+    ],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+        sourceMap: true,
+        declaration: true,
+      }),
+      terser(),
+    ],
+    external: [],
+  },
+];
