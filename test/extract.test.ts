1bc33ce13697807e8a60414ba5fecd29ee487919
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-05-18 23:19:07 +0200

    Fix tests

--- test/extract.test.ts
+++ test/extract.test.ts
@@ -30,10 +30,10 @@ import { it, expect } from 'vitest'
 import { extractValue } from '../src/extract.js'
 
 it('extracts value of correct type', () => {
-	expect(extractValue('[ 1, 2 ]', 2, ']')).toStrictEqual([ 1, 4 ])
-	expect(extractValue('[ "uwu", 2 ]', 2, ']')).toStrictEqual([ 'uwu', 8 ])
-	expect(extractValue('[ {}, 2 ]', 2, ']')).toStrictEqual([ {}, 5 ])
-	expect(extractValue('[ 2 ]', 2, ']')).toStrictEqual([ 2, 4 ])
+	expect(extractValue('[ 1, 2 ]', 2, { end: ']' })).toStrictEqual([ 1, 4 ])
+	expect(extractValue('[ "uwu", 2 ]', 2, { end: ']' })).toStrictEqual([ 'uwu', 8 ])
+	expect(extractValue('[ {}, 2 ]', 2, { end: ']' })).toStrictEqual([ {}, 5 ])
+	expect(extractValue('[ 2 ]', 2, { end: ']' })).toStrictEqual([ 2, 4 ])
 	expect(extractValue('2\n', 0)).toStrictEqual([ 2, 1 ])
 
 	expect(extractValue('"""uwu"""\n', 0)).toStrictEqual([ 'uwu', 9 ])
