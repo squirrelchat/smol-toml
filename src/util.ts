df2df41e5f20a3f8146214584b87030c0425d5ca
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-05-18 22:19:38 +0200

    Reimplement types to allow changing the leaves

--- src/util.ts
+++ src/util.ts
@@ -29,10 +29,10 @@
 import type { TomlDate } from './date.js'
 import { TomlError } from './error.js'
 
-export type TomlPrimitive = string | number | bigint | boolean | TomlDate
-export type TomlTable = { [Key in string]: TomlValue } & { [Key in string]?: TomlValue | undefined }
-export type TomlArray = TomlValue[] | readonly TomlValue[]
-export type TomlValue = TomlPrimitive | TomlArray | TomlTable
+export type TomlPrimitive = string | number | bigint | boolean | TomlDate;
+export type TomlTable<Primitive = TomlPrimitive> = {[Key in string]: TomlValue<Primitive>} & {[Key in string]?: TomlValue<Primitive>|undefined};
+export type TomlArray<Primitive = TomlPrimitive> = TomlValue<Primitive>[] | readonly TomlValue<Primitive>[];
+export type TomlValue<Primitive = TomlPrimitive> = Primitive | TomlArray<Primitive> | TomlTable<Primitive>;
 
 export function indexOfNewline (str: string, start = 0, end = str.length) {
 	let idx = str.indexOf('\n', start)
