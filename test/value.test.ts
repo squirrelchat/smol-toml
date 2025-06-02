76ba55c938b41e2722ef7bdd71c6b3e3d5c00f8b
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-06-02 07:55:39 +0200

    Fix incorrect testing

--- test/value.test.ts
+++ test/value.test.ts
@@ -62,25 +62,25 @@ it('parses binary integers', () => {
 	expect(parseValue('0b1101_0110', '', 0, "number_or_error")).toBe(0b11010110)
 })
 
-it('rejects numbers too large on number_or_error', () => {
+it('rejects integers too large on number_or_error', () => {
 	expect(() => parseValue('9007199254740992', '', 0, "number_or_error")).toThrowError(TomlError)
 })
 
-it('supports numbers larger than the max safe integer number when bigints are enabled', () => {
+it('supports integers larger than the max safe integer number when bigints are enabled', () => {
 	expect(parseValue('9007199254740992', '', 0, "number_or_bigint")).toBe(9007199254740992n)
 	expect(parseValue('9007199254740992', '', 0, "integers_as_bigints")).toBe(9007199254740992n)
 })
 
-it('supports numbers larger than the number max value when bigints are enabled', () => {
+it('supports integers larger than the Number max value when bigints are enabled', () => {
 	const nineRepeat = '9'.repeat(310)
 	expect(parseValue(nineRepeat, '', 0, "number_or_bigint")).toBe(BigInt(nineRepeat))
 	expect(parseValue(nineRepeat, '', 0, "integers_as_bigints")).toBe(BigInt(nineRepeat))
 })
 
-it('rejects floats too large', () => {
-	expect(() => parseValue('9007199254740992.0', '', 0, "number_or_error")).toThrowError(TomlError)
-	expect(() => parseValue('9007199254740992.0', '', 0, "number_or_bigint")).toThrowError(TomlError)
-	expect(() => parseValue('9007199254740992.0', '', 0, "integers_as_bigints")).toThrowError(TomlError)
+it('supports floats larger than the max safe integer number', () => {
+	expect(parseValue('9007199254740992.0', '', 0, "number_or_error")).toBe(9007199254740992.0)
+	expect(parseValue('9007199254740992.0', '', 0, "number_or_bigint")).toBe(9007199254740992.0)
+	expect(parseValue('9007199254740992.0', '', 0, "integers_as_bigints")).toBe(9007199254740992.0)
 })
 
 it('rejects leading zeroes', () => {
