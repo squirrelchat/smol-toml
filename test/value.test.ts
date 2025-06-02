16c605e9f52e195d7ea8b3a5c2bcfccca141ff72
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-06-02 08:04:21 +0200

    Add test

--- test/value.test.ts
+++ test/value.test.ts
@@ -83,6 +83,13 @@ it('supports floats larger than the max safe integer number', () => {
 	expect(parseValue('9007199254740992.0', '', 0, "integers_as_bigints")).toBe(9007199254740992.0)
 })
 
+it('supports floats larger than the Number max value', () => {
+	const nineRepeatFloat = '9'.repeat(310) + ".0"
+	expect(parseValue(nineRepeatFloat, '', 0, "number_or_error")).toBe(+nineRepeatFloat)
+	expect(parseValue(nineRepeatFloat, '', 0, "number_or_bigint")).toBe(+nineRepeatFloat)
+	expect(parseValue(nineRepeatFloat, '', 0, "integers_as_bigints")).toBe(+nineRepeatFloat)
+})
+
 it('rejects leading zeroes', () => {
 	expect(() => parseValue('0123', '', 0, "number_or_error")).toThrowError(TomlError)
 	expect(() => parseValue('01.10', '', 0, "number_or_error")).toThrowError(TomlError)
