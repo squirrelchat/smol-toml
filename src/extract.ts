2ba9a6e579ce1b890b6a5c2361627d10eb33c978
Roman <dev@rman.dev>
2026-07-09 16:48:01 +0100

    fix: reject incorrect whitespace according to the spec
    https://github.com/toml-lang/toml-test/blob/main/tests/invalid/control/linetab-number-01.toml

--- src/extract.ts
+++ src/extract.ts
@@ -42,7 +42,7 @@ function sliceAndTrimEndOf(str: string, startPtr: number, endPtr: number): [stri
 		value = value.slice(0, commentIdx)
 	}
 
-	return [value.trimEnd(), commentIdx]
+	return [value.replace(/[ \t]+$/, ""), commentIdx]
 }
 
 export function extractValue(str: string, ptr: number, end: string | undefined, depth: number, integersAsBigInt: IntegersAsBigInt): [TomlValue, number] {
